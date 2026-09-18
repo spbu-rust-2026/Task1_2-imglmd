3de8f0017b1a9d966bf601aa1708b795d490d756
kir <dangdatmini@gmail.com>
2026-09-18 21:12:19 +0300

    fix format

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-use std::io;
+use std::io::{self, Read};
 
 fn main() {
-    let mut sum: i64 = 0;
-    loop {
-        let mut line = String::new();
-        io::stdin().read_line(&mut line).unwrap();
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
 
-        let num: i64 = match line.trim().parse() {
+    let mut sum: u128 = 0;
+
+    for token in input.split_whitespace() {
+        if token == "-1" {
+            break;
+        }
+
+        let num: u128 = match token.parse() {
             Ok(n) => n,
             Err(_) => {
                 println!("NaN");
                 return;
             }
         };
-        if num == -1 {
-            break;
-        }
-        if num <= 0 {
+
+        if num == 0 {
             println!("NaN");
             return;
         }
-        sum += num
+
+        sum += num;
     }
-    println!("{}", sum)
+
+    println!("{}", sum);
 }
