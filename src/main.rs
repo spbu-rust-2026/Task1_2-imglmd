d3e40bd2985803587e8c667f21e5fe9a9910f126
kir <dangdatmini@gmail.com>
2026-09-18 21:07:08 +0300

    homework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sum: i64 = 0;
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        let num: i64 = match line.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("NaN");
+                return;
+            }
+        };
+        if num == -1 {
+            break;
+        }
+        if num <= 0 {
+            println!("NaN");
+            return;
+        }
+        sum += num
+    }
+    println!("{}", sum)
+
 }
