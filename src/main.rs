a84009eb95a5513d01f4a7b799c5fc202ec9df6d
kir <dangdatmini@gmail.com>
2026-09-18 21:16:18 +0300

    ultra super last fix

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
-use std::io::{self, Read};
+use std::io;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
+    let mut sum: i128 = 0;
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
 
-    let mut sum: u128 = 0;
-
-    for token in input.split_whitespace() {
-        if token == "-1" {
-            break;
-        }
-
-        let num: u128 = match token.parse() {
+        let num: i128 = match line.trim().parse() {
             Ok(n) => n,
             Err(_) => {
                 println!("NaN");
                 return;
             }
         };
-
-        if num == 0 {
+        if num == -1 {
+            break;
+        }
+        if num <= 0 {
             println!("NaN");
             return;
         }
-
-        sum += num;
+        sum += num
     }
-
-    println!("{}", sum);
+    println!("{}", sum)
 }
